b8e69494489a786a682b435aeac851bc8a6e997b
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-07 22:07:27 -0700

    Initial commit

---
+++ src/LeftRightMostIndex.java
@@ -0,0 +1,10 @@
+public class LeftRightMostIndex {
+
+    class pair{
+        long data, data2;
+        pair(long data, long data2){
+            this.data = data;
+            this.data2 = data2;
+        }
+    }
+}
