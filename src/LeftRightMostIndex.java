69e6aef4a4075e8390378dcd7b454296a3f01882
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-07 22:24:20 -0700

    created a loop for the first element for the pair

--- src/LeftRightMostIndex.java
+++ src/LeftRightMostIndex.java
@@ -1,10 +1,36 @@
 public class LeftRightMostIndex {
 
-    class pair{
-        long data, data2;
+    static class pair{
+       static long first, second;
         pair(long data, long data2){
-            this.data = data;
-            this.data2 = data2;
+            this.first = data;
+            this.second = data2;
         }
     }
+
+
+    public static pair rightLeftMostIndex(long arr [], long n){
+        int low = 0;
+        int high = arr.length-1;
+
+        while(low <= high){
+            int mid = (low + high)/2;
+            if(arr[mid]== n){
+                pair.first = mid;
+                high = mid-1;
+            }else if (arr[mid]<n){
+                low = mid +1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+
+
+    }
+
+        public static void main(String[] args) {
+
+        }
+
 }
